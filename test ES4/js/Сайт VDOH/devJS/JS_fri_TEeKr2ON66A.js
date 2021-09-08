b764563f3f996ff86c398d819b85a97e8fe33bbf
EvGen Gal <ZBst.1@yandex.ru>
2021-09-08 05:25:51 +0300

    ES. 0.11.0 общ. стили --style --theme, рем.кнп, нач.ебл. с adaptive_header при scroll

--- test ES4/js/Сайт VDOH/devJS/JS_fri_TEeKr2ON66A.js
+++ test ES4/js/Сайт VDOH/devJS/JS_fri_TEeKr2ON66A.js
@@ -1,40 +1,43 @@
 // !! JS Размеры, Прокрутка, Координаты. ФриПоЖиз - TEeKr2ON66A
 // ! РАЗМЕРЫ
-// доступные размеры окна браузера, только для чтения (с лево до скрола - тег html). clientWidth и clientHeight
-const mainEl = document.documentElement;
-const mainElWidth = mainEl.clientWidth;
-const mainElHeight = mainEl.clientHeight;
-console.log("доступ. Width:" + mainElWidth);
-console.log("доступ. Height:" + mainElHeight);
-// вместе со скролом
-const windowWidth = window.innerWidth;
-const windowHeight = window.innerHeight;
-console.log("скрол Width:" + windowWidth);
-console.log("скрол Height:" + windowHeight);
-// макс ширина и высота - общий блок в body
-let scrolдlWidth = Math.max(
-  document.body.scrollWidth,
-  document.documentElement.scrollWidth,
-  document.body.offsetWidth,
-  document.documentElement.offsetWidth,
-  document.body.clientWidth,
-  document.documentElement.clientWidth
-);
-let scrolдlHeight = Math.max(
-  document.body.scrollHeight,
-  document.documentElement.scrollHeight,
-  document.body.offsetHeight,
-  document.documentElement.offsetHeight,
-  document.body.clientHeight,
-  document.documentElement.clientHeight
-);
-console.log("весь Width:" + scrolдlWidth);
-console.log("весь Height:" + scrolдlHeight);
-// получ кол-во прокруч пикселей. только для чтения
-const scrollTop = window.pageYOffset;
-const scrollLeft = window.pageXOffset;
-console.log("scrollTop:" + scrollTop);
-console.log("scrollLeft:" + scrollLeft);
+function sizeFri() {
+  // доступные размеры окна браузера, только для чтения (с лево до скрола - тег html). clientWidth и clientHeight
+  const mainEl = document.documentElement;
+  const mainElWidth = mainEl.clientWidth;
+  const mainElHeight = mainEl.clientHeight;
+  console.log("доступ. Width:" + mainElWidth);
+  console.log("доступ. Height:" + mainElHeight);
+  // вместе со скролом
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  console.log("скрол Width:" + windowWidth);
+  console.log("скрол Height:" + windowHeight);
+  // макс ширина и высота - общий блок в body
+  let scrolдlWidth = Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.body.clientWidth,
+    document.documentElement.clientWidth
+  );
+  let scrolдlHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+  console.log("весь Width:" + scrolдlWidth);
+  console.log("весь Height:" + scrolдlHeight);
+  // получ кол-во прокруч пикселей. только для чтения
+  const scrollTop = window.pageYOffset;
+  const scrollLeft = window.pageXOffset;
+  console.log("scrollTop:" + scrollTop);
+  console.log("scrollLeft:" + scrollLeft);
+}
+// sizeFri()
 
 // ! УПРАВ ПРОКРУТКОЙ СТРАНИЦЫ
 // добавл к текущему
@@ -49,12 +52,14 @@ function setScrollBy() {
 }
 // прокруч на абсолют коорд (pageX, pageY). тоже самое что scroll()
 function setScrollTo() {
-  window.scrollTo(0, 150);
+  window.scrollTo(0, 1870);
+  // чтоб прижать .header(position:fixed) добавил .empty_block. теперь x0 смещён на 150px
+  // window.scrollTo(0, 0);
 }
 // scrollTo с опциями. не раб в safary. позиции и прокрутка - плавно, авто
 function setScrollToOpt() {
   window.scrollTo({
-    top: 1870,
+    top: 75,
     left: 0,
     behavior: "smooth",
   });
@@ -66,18 +71,23 @@ function setScrollIntoViewHead() {
     behavior: "smooth",
   });
 }
-function setScrollIntoViewFoot() {
+function setScrollIntoViewOpt() {
   const footerScroll = document.querySelector(".centr-cont-");
   footerScroll.scrollIntoView(
     // ???не раб - false с опц не выводит вниз, раб как при top=true
     {
-      top: false,
+      // top: false,
+      // top: true,
+      // block: "nearest",
+      block: "start",
       behavior: "smooth",
     }
   );
+  // попыька .empty_block.
+  // )- 150 компенс  + 'px';
 }
 // scrollIntoView с опциями. не раб в safary
-function setScrollIntoViewOpt() {
+function setScrollIntoViewFoot() {
   const centrText = document.querySelector("footer");
   centrText.scrollIntoView({
     // block - вертикаль позиция. center(умолч), start, end, nearest - до `ближайший` видимости
@@ -103,8 +113,8 @@ function setEnableDesableScroll() {
 function clickMinAside2() {
   const mainEl = document.documentElement;
   const mainElHeight = mainEl.clientHeight;
-  console.log("доступ. Width:" + mainElWidth);
-  console.log("доступ. Height:" + mainElHeight);
+  // console.log("доступ. Width:" + mainElWidth);
+  // console.log("доступ. Height:" + mainElHeight);
   let miniAside = document.querySelectorAll(".mini-aside");
   miniAside.document.body.style.left = function () {
     // calc(100% / clientHeight(););
@@ -114,7 +124,7 @@ function clickMinAside2() {
   };
 }
 // clickMinAside2();
-// нажатие на кнопки .mini-aside
+// ! нажатие на кнопки .mini-aside(мини меню с боку на скролах)
 function clickMinAside() {
   let jsScroll = document.querySelectorAll(".ma-bl__js-scroll");
   jsScroll.forEach(function (btn) {
@@ -130,13 +140,184 @@ function clickMinAside() {
       //   setScrollBy();
       // }
       if (btn == jScr1) setScrollBy();
-      if (btn == jScr2) setScrollTo();
+      if (btn == jScr2) setScrollIntoViewHead();
       if (btn == jScr3) setScrollToOpt();
-      if (btn == jScr4) setScrollIntoViewHead();
-      if (btn == jScr5) setScrollIntoViewFoot();
-      if (btn == jScr6) setScrollIntoViewOpt();
+      if (btn == jScr4) setScrollIntoViewOpt();
+      if (btn == jScr5) setScrollTo();
+      if (btn == jScr6) setScrollIntoViewFoot();
       if (btn == jScr7) setEnableDesableScroll();
     });
   });
 }
 clickMinAside();
+
+// !
+function scrolHead() {
+  // !1
+  var headerLogo = document.querySelector(".header-logo");
+  var headerMenu = document.querySelector(".header-menu");
+  var headerMenuTop = document.querySelector(".header-menu__top");
+  var headerMenuBottom = document.querySelector(".header-menu__bottom");
+  var headerLang = document.querySelector(".header-menu__icon");
+  var headerBurger = document.querySelector(".header-burger");
+  // !1
+  var header = document.querySelector("header");
+  var helloys = document.querySelector(".helloys");
+  document.addEventListener("scroll", () => {
+    // if (window.pageYOffset >= 150) {
+    //   header.classList.add("fixed_menu");
+    // } else {
+    //   header.classList.remove("fixed_menu");
+    // }
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled >= 75) {
+      console.log(0);
+      header.style.height = "50px";
+      helloys.style.paddingTop = "150px";
+      header.classList.add("fixed_menu");
+      // !2
+      // headerMenuTop.classList.add("done").appendChild(headerBurger)
+      let hedDon = headerMenuTop.classList.add("done");
+      hedDon.append(headerBurger);
+      // headerMenuTop.classList.add("done").insertAdjacentHTML(headerBurger)
+      // !2
+    } else {
+      header.style.height = "150px";
+      helloys.style.paddingTop = "0px";
+      header.classList.remove("fixed_menu");
+    }
+  });
+}
+scrolHead();
+
+// !!
+// function adaptive_header(w, h) {
+function adaptive_header() {
+  console.log("adaptheader");
+  var headerLogo = document.querySelector(".header-logo");
+  var headerMenu = document.querySelector(".header-menu");
+  var headerMenuTop = document.querySelector(".header-menu__top");
+  var headerMenuBottom = document.querySelector(".header-menu__bottom");
+  var headerLang = document.querySelector(".header-menu__icon");
+  var headerBurger = document.querySelector(".header-burger");
+
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  console.log(1);
+  if (scrolled >= 75) {
+    console.log(2);
+    // е\и в языках нет класса done
+    if (!headerMenuTop.classList.contains("done")) {
+      console.log(3);
+      // то блоку header-top-lang добавит класс "done" и добавить его в блок header-burger
+      // headerLang.addClass("done").appendTo(headerBurger);
+      // headerMenuTop.classList.add("done").insertAdjacentHTML(headerBurger)//.appendTo(headerBurger);
+      headerMenuTop.classList.add("done").appendChild(headerBurger); //.appendTo(headerBurger);
+      console.log(4);
+    }
+    console.log(5);
+  }
+  // else {
+  //   // е\и в языках есть класса done
+  //   if (headerLang.hasClass("done")) {
+  //     // то у блока headerLang добавит класс "done" и добавить его в начало блока (prepend - `перед именем`) header-top
+  //     headerLang.removeClass("done").prependTo($(".header-top"));
+  //   }
+  // }
+
+  // е\и ширина меньше 767
+  // if (w < 767) {
+  //   // е\и у меню нет класса done
+  //   if (!headerMenu.hasClass("done")) {
+  //     // то блоку header-bottom-menu добавит класс "done" и добавить его в блок header-burger
+  //     headerMenu.addClass("done").appendTo(headerBurger);
+  //   }
+  // } else {
+  //   // перебираем все элементы each (перебор `каждого` элем. коллекции jQuery, выполняя при этом функцию для каждого из них)
+  //   $.each(headerMenu, function (index, val) {
+  //     // е/и у эл. класс --right
+  //     if ($(this).hasClass("header-bottom-menu--right")) {
+  //       // е/и есть класс done
+  //       if ($(this).hasClass("done")) {
+  //         // у этих эл.
+  //         $(this)
+  //           //удалить done
+  //           .removeClass("done")
+  //           // и переместить в header-bottom__column в позицию 3 (так как индекс 2, счёт 3)
+  //           .prependTo($(".header-bottom__column").eq(2));
+  //       }
+  //     } else {
+  //       // е/и
+  //       if ($(this).hasClass("done")) {
+  //         $(this)
+  //           //удалить done
+  //           .removeClass("done")
+  //           // и переместить в header-bottom__column в позицию 1 (так как индекс 0, счёт 1)
+  //           .prependTo($(".header-bottom__column").eq(0));
+  //       }
+  //     }
+  //   });
+  // }
+  // !!!
+
+  // // блок скрытого бургера
+  // var headerBurger = $(".header-burger");
+  // // класс языков
+  // var headerLang = $(".header-top-lang");
+  // // класс меню
+  // var headerMenu = $(".header-bottom-menu");
+  // // е\и ширина меньше 767
+  // if (w < 767) {
+  //   // е\и в языках нет класса done
+  //   if (!headerLang.hasClass("done")) {
+  //     // то блоку header-top-lang добавит класс "done" и добавить его в блок header-burger
+  //     headerLang.addClass("done").appendTo(headerBurger);
+  //   }
+  // } else {
+  //   // е\и в языках есть класса done
+  //   if (headerLang.hasClass("done")) {
+  //     // то у блока headerLang добавит класс "done" и добавить его в начало блока (prepend - `перед именем`) header-top
+  //     headerLang.removeClass("done").prependTo($(".header-top"));
+  //   }
+  // }
+  // // е\и ширина меньше 767
+  // if (w < 767) {
+  //   // е\и у меню нет класса done
+  //   if (!headerMenu.hasClass("done")) {
+  //     // то блоку header-bottom-menu добавит класс "done" и добавить его в блок header-burger
+  //     headerMenu.addClass("done").appendTo(headerBurger);
+  //   }
+  // } else {
+  //   // перебираем все элементы each (перебор `каждого` элем. коллекции jQuery, выполняя при этом функцию для каждого из них)
+  //   $.each(headerMenu, function (index, val) {
+  //     // е/и у эл. класс --right
+  //     if ($(this).hasClass("header-bottom-menu--right")) {
+  //       // е/и есть класс done
+  //       if ($(this).hasClass("done")) {
+  //         // у этих эл.
+  //         $(this)
+  //           //удалить done
+  //           .removeClass("done")
+  //           // и переместить в header-bottom__column в позицию 3 (так как индекс 2, счёт 3)
+  //           .prependTo($(".header-bottom__column").eq(2));
+  //       }
+  //     } else {
+  //       // е/и
+  //       if ($(this).hasClass("done")) {
+  //         $(this)
+  //           //удалить done
+  //           .removeClass("done")
+  //           // и переместить в header-bottom__column в позицию 1 (так как индекс 0, счёт 1)
+  //           .prependTo($(".header-bottom__column").eq(0));
+  //       }
+  //     }
+  //   });
+  // }
+}
+adaptive_header();
+
+function adaptive_function() {
+  var w = $(window).outerWidth();
+  var h = $(window).outerHeight();
+  adaptive_header(w, h);
+}
+// adaptive_function();
