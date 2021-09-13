13c65f9d6e29e21f27528dfe258edb29e05e1872
EvGen Gal <ZBst.1@yandex.ru>
2021-09-13 21:03:00 +0300

    ES. 0.11.4 общ. fn () для --size, общ. клас, перем, почти всё.

--- test ES4/js/Сайт VDOH/devJS/parseTheme.js
+++ test ES4/js/Сайт VDOH/devJS/parseTheme.js
@@ -1,180 +1,152 @@
-// 7 своё
-// !!!!! настройки стиля-цвета
-function generalFunctionOfColorSchemes(){
-// ! 2
-// !!! https://www.kooslooijesteijn.net/blog/add---theme-dark-to-website
-// ??? не раб - Травник - настроить выборку стандарт ОС
-// Проверяем предпочтение тёмного режима на уровне ОС
-const prefDark = window.matchMedia("(prefers-color-scheme: dark)");
-const preflight = window.matchMedia("(prefers-color-scheme: light)");
-const prefNo = window.matchMedia("(prefers-color-scheme: no-preference)");
-const NoCurrentTheme = localStorage.getItem("--theme") == null;
-if (NoCurrentTheme) {
-  console.log(11);
-  if (prefDark) {
-    console.log(10);
-    // var theme = document.body.classList.contains("--theme-dark");
-    // document.body.classList.contains("_-theme-dark");
-    var theme = "_dark";
-    console.log(9);
-    // return "dark";
-  } else if (preflight) {
-    var theme = "_light";
-    console.log(8);
-  } else if (prefNo) {
-    var theme = "_neutral";
-  }
-  localStorage.setItem("--theme", theme);
-}
-// !! опред ОС
-// !!! 1. https://webref.ru/dev/--theme-dark/os-level
-// !!! 2. https://www.kooslooijesteijn.net/blog/add---theme-dark-to-website
-// Получаем предпочтение темы пользователя из локального хранилища, если оно доступно
-const currentTheme = localStorage.getItem("--theme");
-// Если текущая тема в localStorage равна "neutral"
-if (currentTheme == "_neutral") {
-  //  if (currentTheme == "neutral" || NoCurrentTheme) {
-  // вкл класс .--theme_neutral для <body>
-  document.body.classList.add("--theme_neutral");
-} else if (currentTheme == "_dark") {
-  document.body.classList.add("--theme_dark");
-} else if (currentTheme == "_light") {
-  document.body.classList.add("--theme_light");
-}
-// Выбираем все кнопки на странице и получаем массив
-var swLabelAll = document.querySelectorAll(".switcher-label");
-// Проходим по массиву
-swLabelAll.forEach(function (btn) {
-  // заводим перем для позиций кнопок
-  const swDark = document.querySelector(".switcher__dark");
-  const swNeut = document.querySelector(".switcher__neutral");
-  const swLight = document.querySelector(".switcher__light");
-  // Вешаем событие клик
-  console.log(1);
-  btn.addEventListener("click", function () {
-    console.log(2);
-    if (btn == swDark) {
-      // if (btn == swDark || currentTheme == "dark" || currentTheme == "null") {
-      console.log("o");
-      document.body.classList.remove("--theme_light");
-      document.body.classList.remove("--theme_neutral");
-      document.body.classList.add("--theme_dark");
-      var theme = "_dark";
-    }
+import { fn } from "jquery";
 
-    if (btn == swNeut) {
-      console.log("~");
-      document.body.classList.remove("--theme_light");
-      document.body.classList.remove("--theme_dark");
-      document.body.classList.add("--theme_neutral");
+// ! <настройки стиля-цвета(--theme:dark,neutral,light)>˅=======================================================================================˅
+function generalFunctionOfColorSchemes() {
+  // ! https://webref.ru/dev/dark-mode
+  // ! https://www.kooslooijesteijn.net/blog/add-dark-mode-to-website
+  // Проверяем предпочтение тёмного режима на уровне ОС
+  const prefDark = window.matchMedia("(prefers-color-scheme: dark)");
+  const preflight = window.matchMedia("(prefers-color-scheme: light)");
+  const prefNo = window.matchMedia("(prefers-color-scheme: no-preference)");
+  const NoCurrentTheme = localStorage.getItem("--theme") == null;
+  if (NoCurrentTheme) {
+    if (prefDark) {
+      // var theme = document.body.classList.contains("--theme-dark");
+      // document.body.classList.contains("_-theme-dark");
+      var theme = "_dark";
+      // return "dark";
+    } else if (preflight) {
+      var theme = "_light";
+    } else if (prefNo) {
       var theme = "_neutral";
     }
-    if (btn == swLight) {
-      console.log("+");
-      document.body.classList.remove("--theme_dark");
-      document.body.classList.remove("--theme_neutral");
-      document.body.classList.add("--theme_light");
-      var theme = "_light";
-    }
     localStorage.setItem("--theme", theme);
+  }
+  // Получаем предпочтение темы пользователя из локального хранилища, если оно доступно
+  const currentTheme = localStorage.getItem("--theme");
+  // Если текущая тема в localStorage равна "neutral"
+  if (currentTheme == "_neutral") {
+    //  if (currentTheme == "neutral" || NoCurrentTheme) {
+    // вкл класс .--theme_neutral для <body>
+    document.body.classList.add("--theme_neutral");
+  } else if (currentTheme == "_dark") {
+    document.body.classList.add("--theme_dark");
+  } else if (currentTheme == "_light") {
+    document.body.classList.add("--theme_light");
+  }
+  // Выбираем все кнопки на странице и получаем массив
+  var swLabelAll = document.querySelectorAll(".switcher-label");
+  // Проходим по массиву
+  swLabelAll.forEach(function (btn) {
+    // заводим перем для позиций кнопок
+    const swDark = document.querySelector(".switcher__dark");
+    const swNeut = document.querySelector(".switcher__neutral");
+    const swLight = document.querySelector(".switcher__light");
+    // Вешаем событие клик
+    btn.addEventListener("click", function () {
+      // if (btn == swDark || currentTheme == "dark" || currentTheme == "null") {
+      if (btn == swDark) {
+        console.log("o");
+        document.body.classList.remove("--theme_light");
+        document.body.classList.remove("--theme_neutral");
+        document.body.classList.add("--theme_dark");
+        var theme = "_dark";
+      }
+      if (btn == swNeut) {
+        console.log("~");
+        document.body.classList.remove("--theme_light");
+        document.body.classList.remove("--theme_dark");
+        document.body.classList.add("--theme_neutral");
+        var theme = "_neutral";
+      }
+      if (btn == swLight) {
+        console.log("+");
+        document.body.classList.remove("--theme_dark");
+        document.body.classList.remove("--theme_neutral");
+        document.body.classList.add("--theme_light");
+        var theme = "_light";
+      }
+      localStorage.setItem("--theme", theme);
+    });
   });
-});
-// !!!!! настройки стиля-цвета
 }
-generalFunctionOfColorSchemes()
-//</.m-s-switcher-5(3 позиции - горизонт)>˄=======================================================================================˄
+generalFunctionOfColorSchemes();
+// ! </настройки стиля-цвета(--theme:dark,neutral,light)>˄=======================================================================================˄
 
 // ??? не раб - Травник - понять почему не взаимосвязаны label и input. связать в общее переключение
-// <.m-s-switcher-7(3 позиции - круг)>˅=======================================================================================˅
 
-// !!!!! настройки стиля-размера
+// ??? не раб - Травник - сделать взаимосвязь кнопок и тем. при настройки станд ОС и Размеров они различались
 
-// !!! ПОПРОБОВАТЬ - Сокращение количества анимации
-// @media screen and (prefers-reduced-motion: reduce) {
-//   .pulse {
-//     animation: none;
-//   }
-// }
-// ??? не раб - Травник - настроить выборку стандарт размеров --size(можно через reduced-motion)
-// const prefDark = window.matchMedia("(prefers-reduced-motion: big)");
-// const prefRebuce = window.matchMedia("(prefers-reduced-motion: reduce)");
-// const prefNoPref = window.matchMedia("(prefers-reduced-motion: no-preference)");
-// // const prefNo = window.matchMedia("(prefers-reduced-motion: no-preference)");
-// if (prefRebuce) {
-//   var theme = document.body.classList.contains("_size-mid");
-//   var theme = "_off";
-// } else if (prefNoPref) {
-//   var theme = document.body.classList.contains("_size-mid"||"_size-mid"||"_size-mid");
-//   var theme = "_mid";
-//   // return "light";
-// }
-// // else if (prefNo) {
-// //   var theme = document.body.classList.contains("--theme-neutral");
-// //   var theme = "_neutral";
-// //   // return "neutral";
-// // }
-// localStorage.setItem("style", style);
-
-localStorage.setItem("_size", "mid");
-const currentStyle = localStorage.getItem("_size");
-if (currentStyle == "mid" || currentStyle == "") {
-  document.body.classList.add("_size-mid");
-} else if (currentStyle == "big") {
-  document.body.classList.add("_size-big");
-} else if (currentStyle == "small") {
-  document.body.classList.add("_size-small");
-} else if (currentStyle == "off") {
-  document.body.classList.add("_size-off");
-}
-var sw7LabelAll = document.querySelectorAll(".sw7-label");
-sw7LabelAll.forEach(function (btn) {
-  // let style = document.body.classList.add("_size-mid");
-  // localStorage.setItem("style", style);
-  // !
-  // !
-  const sw7big = document.querySelector(".sw7-big");
-  const sw7mid = document.querySelector(".sw7-mid");
-  const sw7small = document.querySelector(".sw7-small");
-  const sw7off = document.querySelector(".sw7-off");
-  // const sw7big = document.querySelector("#--style-state-big");
-  // const sw7Of = document.querySelectorById("--style-state-big");
-  btn.addEventListener("click", function () {
-    if (btn == sw7big) {
-      document.body.classList.remove("_size-mid");
-      document.body.classList.remove("_size-small");
-      document.body.classList.remove("_size-off");
-      document.body.classList.add("_size-big");
-      var style = "_big";
+// ! <настройки стиля-размера(--size:big,mid,small,off)>˅=======================================================================================˅
+function generalFunctionOfReducedMotion() {
+  const prefRebuce = window.matchMedia("(prefers-reduced-motion: reduce)");
+  const prefNoPref = window.matchMedia(
+    "(prefers-reduced-motion: no-preference)"
+  );
+  const NoCurrentTheme2 = localStorage.getItem("--size") == null;
+  if (NoCurrentTheme2) {
+    if (prefNoPref) {
+      var size = "_mid";
+    } else if (prefRebuce) {
+      var size = "_off";
     }
-    if (btn == sw7mid) {
-      document.body.classList.remove("_size-big");
-      document.body.classList.remove("_size-small");
-      document.body.classList.remove("_size-off");
-      document.body.classList.add("_size-mid");
-      var style = "_mid";
-    }
-    if (btn == sw7small) {
-      document.body.classList.remove("_size-big");
-      document.body.classList.remove("_size-mid");
-      document.body.classList.remove("_size-off");
-      document.body.classList.add("_size-small");
-      var style = "__small";
-    }
-    if (btn == sw7off) {
-      document.body.classList.remove("_size-big");
-      document.body.classList.remove("_size-mid");
-      document.body.classList.remove("_size-small");
-      document.body.classList.add("_size-off");
-      var style = "_off";
-    }
-    localStorage.setItem("_size", style);
+    localStorage.setItem("--size", size);
+  }
+  const currentStyle = localStorage.getItem("--size");
+  if (currentStyle == "_mid") {
+    document.body.classList.add("--size_mid");
+  } else if (currentStyle == "_big") {
+    document.body.classList.add("--size_big");
+  } else if (currentStyle == "_small") {
+    document.body.classList.add("--size_small");
+  } else if (currentStyle == "_off") {
+    document.body.classList.add("--size_off");
+  }
+  var sw7LabelAll = document.querySelectorAll(".sw7-label");
+  sw7LabelAll.forEach(function (btn) {
+    const sw7big = document.querySelector(".sw7__big");
+    const sw7mid = document.querySelector(".sw7__mid");
+    const sw7small = document.querySelector(".sw7__small");
+    const sw7off = document.querySelector(".sw7__off");
+    btn.addEventListener("click", function () {
+      if (btn == sw7big) {
+        document.body.classList.remove("--size_mid");
+        document.body.classList.remove("--size_small");
+        document.body.classList.remove("--size_off");
+        document.body.classList.add("--size_big");
+        var size = "_big";
+      }
+      if (btn == sw7mid) {
+        document.body.classList.remove("--size_big");
+        document.body.classList.remove("--size_small");
+        document.body.classList.remove("--size_off");
+        document.body.classList.add("--size_mid");
+        var size = "_mid";
+      }
+      if (btn == sw7small) {
+        document.body.classList.remove("--size_big");
+        document.body.classList.remove("--size_mid");
+        document.body.classList.remove("--size_off");
+        document.body.classList.add("--size_small");
+        var size = "_small";
+      }
+      if (btn == sw7off) {
+        document.body.classList.remove("--size_big");
+        document.body.classList.remove("--size_mid");
+        document.body.classList.remove("--size_small");
+        document.body.classList.add("--size_off");
+        var size = "_off";
+      }
+      localStorage.setItem("--size", size);
+    });
   });
-});
-// !!!!! настройки стиля-размера
+}
+generalFunctionOfReducedMotion();
+// ! </настройки стиля-размера(--size:big,mid,small,off)>˄=======================================================================================˄
 
-// ! сохранить в localStorage значения нескольких input[radio]
-// ! https://qna.habr.com/q/555513
+// ! <сохран checked в localStorage(массив input[type=radio])>˅=======================================================================================˅
 function saveCheckedToLocalStorage(selector) {
+  // ! https://qna.habr.com/q/555513
   // не дублируем код
   function save(data) {
     localStorage.setItem(selector, JSON.stringify(data));
@@ -213,7 +185,4 @@ function saveCheckedToLocalStorage(selector) {
   // }
 }
 saveCheckedToLocalStorage("input[type=radio]");
-
-// ??? не раб - Травник - сделать взаимосвязь кнопок и тем. при настройки станд ОС и Размеров они различались
-
-// ??? не раб - Травник -
+// ! </сохран checked в localStorage(массив input[type=radio])>˄=======================================================================================˄
